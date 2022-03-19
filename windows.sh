f7e6f96eaa78429f1ecba70f2eb22cdf4ebcaccb
紀元翔 <corbin50111@gmail.com>
2022-03-20 00:33:12 +0800

    add windows

---
+++ windows.sh
@@ -0,0 +1,14 @@
+# IDE
+choco install vscode
+# software
+choco install git
+choco install googlechrome
+choco install python
+choco install nodejs
+choco install composer
+choco install virtualbox
+choco install vagrant
+choco install mysql
+# others
+choco install line
+choco install streamlink-twitch-gui
